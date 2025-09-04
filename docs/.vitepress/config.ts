b9d36a9f87536207fc10bcac659db9cab8b6baa6
muhammad-hf <yudisatreidas@gmail.com>
2025-09-04 09:25:54 +0700

    build: vitepress site

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   lang: 'en-US',
   title: 'Muhammad Hanif',
   description: 'Vite & Vue powered static site generator.',
-  base: '/website',
+  base: '/website/',
 
   themeConfig: {
     nav: [
